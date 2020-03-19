1ed37b9ead902bee4a06ce02a460d1dc52013728
Nobralyra <signees96@gmail.com>
2020-03-19 09:43:56 +0100

    First share of exercise

---
+++ src/main/java/web_shop/exercise/ExerciseApplication.java
@@ -0,0 +1,15 @@
+package web_shop.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web_shop/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,15 @@
+package web_shop.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
