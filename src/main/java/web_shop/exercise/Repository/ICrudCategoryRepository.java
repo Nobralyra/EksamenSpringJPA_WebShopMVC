037ba5ab0c5710272e58aa1434724523d4fa13ca
Nobralyra <signees96@gmail.com>
2020-03-29 18:15:37 +0200

    Removed old imports

--- src/main/java/web_shop/exercise/Repository/ICrudCategoryRepository.java
+++ src/main/java/web_shop/exercise/Repository/ICrudCategoryRepository.java
@@ -3,8 +3,6 @@ package web_shop.exercise.Repository;
 import org.springframework.data.repository.CrudRepository;
 import web_shop.exercise.Model.Category;
 
-import java.util.Optional;
-
 public interface ICrudCategoryRepository extends CrudRepository<Category, Long>
 {
 
