45bf811aabfae8795eee6c4b72c3a43810e7dd33
Nobralyra <signees96@gmail.com>
2020-03-29 18:15:31 +0200

    Removed old imports

--- src/main/java/web_shop/exercise/Repository/ICrudCompanyRepository.java
+++ src/main/java/web_shop/exercise/Repository/ICrudCompanyRepository.java
@@ -1,10 +1,7 @@
 package web_shop.exercise.Repository;
 
 import org.springframework.data.repository.CrudRepository;
-import web_shop.exercise.Model.Category;
 import web_shop.exercise.Model.Company;
-
-import java.util.Optional;
 
 public interface ICrudCompanyRepository extends CrudRepository<Company, Long>
 {
